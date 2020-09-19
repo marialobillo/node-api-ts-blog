d4b2f34183b721862432d57cf461fbd92203b541
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2020-09-19 18:27:03 +0200

    user model

---
+++ build/models/User.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = require("mongoose");
+const UserSchema = new mongoose_1.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    username: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: Date
+});
+exports.default = mongoose_1.model('User', UserSchema);
